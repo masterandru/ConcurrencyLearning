2cfb3a8a3795e1a65d42f8f102d1c5e862fa3ed4
Andruschencko Igor <andruschenko-ip@gamma-center.com>
2016-12-30 12:27:53 +0300

    2016-12-30 - Create new thread in MainThread

--- src/Sample02/NewThread.java
+++ src/Sample02/NewThread.java
@@ -15,13 +15,13 @@ class NewThread implements Runnable {
     public void run() {
         try {
             for (int i = 5; i > 0; i--) {
-                System.out.println("Дoчepний поток: " + i);
+                System.out.println("- Дoчepний поток: " + i);
                 Thread.sleep(500);
             }
         }
         catch (InterruptedException e) {
-           System.out.println("Дoчepний поток прерван.");
-           System.out.println("Дoчepний поток завершен.");
+           System.out.println("- Дoчepний поток прерван.");
+           System.out.println("- Дoчepний поток завершен.");
         }
 
     }
