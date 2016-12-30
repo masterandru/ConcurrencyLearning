c2c8c5e3b0be980689beb065039814a73deb976f
Andruschencko Igor <andruschenko-ip@gamma-center.com>
2016-12-30 14:15:38 +0300

    2016-12-30 - Create Sample03 -  extends Thread

--- src/Sample03/ExtendThread.java
+++ src/Sample03/ExtendThread.java
@@ -4,15 +4,13 @@ public class ExtendThread {
     public static void main(String args[]) {
         new NewТhread(); // создать новый поток исполнения
         try {
-
             for (int i = 5; i > 0; i--) {
-                System.out.println("Глaвн﻾  поток:  " + i);
+                System.out.println("ГЛAВНЫЙ ПОТОК:  " + i);
                 Thread.sleep(1000);
             }
-        } catch (InterruptedException е) {
-            System.out.println("Глaвный поток прерван.");
-            System.out.println("Глaвный поток завершен.");
-
+        } catch (InterruptedException e) {
+            System.out.println("ГЛAВНЫЙ ПОТОК прерван.");
+            System.out.println("ГЛAВНЫЙ ПОТОК завершен.");
         }
     }
 }
